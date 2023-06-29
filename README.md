c86e79c47432a308c3ccdfdb057a302812be5d93
Paolo Geronimo <paolojose.geronimo@ucalgary.ca>
2023-06-28 19:06:05 -0600

    implemented improved linear search algorithm

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # ensf-694-lab2-PJGeronimo
-ENSF 694 Lab 2
+
+Q2: Runtimes for both linear and interpolation algorithms are printed in each run. Generally, the linear algorithm is faster, because the input arrays are relatively small, and are not sorted in the linear algorithm.
+
--- src/iterativeSearch.java
+++ src/iterativeSearch.java
@@ -14,13 +14,13 @@ public class iterativeSearch {
 	public static int interpolationSearch(int [] array, int key) {
 		int low = 0;
 		int high = array.length - 1;
-		int pos; //= (key - array[low]) / (array[high] - array[low]);
-		int mid; //= low + ((high - low) * pos);
+		double pos;
+		int mid;
 		Arrays.sort(array);
 		
 		while (low <= high) {
-			pos = (key - array[low]) / (array[high] - array[low]);
-			mid = low + ((high - low) * pos);
+			pos = ((double)key - array[low]) / ((double)array[high] - array[low]);
+			mid = low + (int)((high - low) * pos);
 			if(key < array[mid]) {
 				high = mid - 1;
 			}
@@ -33,6 +33,20 @@ public class iterativeSearch {
 		}
 		return -1;
 	}
+	
+	public static int linSearchImprove(int [] array, int key) {
+		int j = array.length - 1;
+		for(int i = 0; i < array.length; i++) {
+			if (array[i] == key) {
+				return i;
+			}
+			if (array[j] == key) {
+				return j;
+			} 
+			j--;
+		} return -1;
+	}
+	
 	
 	public static void main(String[] args) {
 		Scanner reader = new Scanner(System.in);
@@ -53,18 +67,26 @@ public class iterativeSearch {
 		double linearStartTime = System.nanoTime();
 		int linearResult = linearSearch(values, searchKey);
 		if (linearResult != -1) {	
-		System.out.println("Linear: Search key found at index " + linearResult);
-		} else System.out.println("Search key not found.");
+			System.out.println("LINEAR:\nSearch key found at index " + linearResult);
+			} else System.out.println("Search key not found.");
 		double linearEndTime = System.nanoTime();
-		System.out.println("Execution time: " + ((linearEndTime- linearStartTime)/1000000) + " milliseconds.");
+		System.out.println("Execution time: " + ((linearEndTime- linearStartTime) / 1000000) + " milliseconds.\n");
 		
 		double interStartTime = System.nanoTime();
 		int interpolationResult = interpolationSearch(values, searchKey);
 		if (interpolationResult != -1) {	
-			System.out.println("Interpolation: Search key found at index " + linearResult);
+			System.out.println("INTERPOLATION:\nSearch key found at index " + linearResult);
 			} else System.out.println("Search key not found.");
 		double interEndTime = System.nanoTime();
-		System.out.println("Execution time: " + ((interEndTime- interStartTime)/1000000) + " milliseconds.");
+		System.out.println("Execution time: " + ((interEndTime- interStartTime) / 1000000) + " milliseconds.\n");
+		
+		double linImpStartTime = System.nanoTime();
+		int linImpResult = linSearchImprove(values, searchKey);
+		if (linImpResult != -1) {	
+			System.out.println("LINEAR IMPROVED:\nSearch key found at index " + linImpResult);
+			} else System.out.println("Search key not found.");
+		double linImpEndTime = System.nanoTime();
+		System.out.println("Execution time: " + ((linImpEndTime- linImpStartTime) / 1000000) + " milliseconds.\n");
 	}
 	
 		
